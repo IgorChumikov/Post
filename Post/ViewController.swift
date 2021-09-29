5ffadbf70f09f19f43f884c02a4dbc16cf609bb3
IgorChumikov <chumikov.igor@mail.ru>
2021-09-29 23:36:36 +0300

    Initial Commit

---
+++ Post/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Post
+//
+//  Created by Игорь Чумиков on 29.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
